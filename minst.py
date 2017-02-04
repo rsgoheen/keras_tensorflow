b5adb3f6a1034fb0e21c3fb2b38b4091d5b3b792
Robert Goheen <goheen@gmail.com>
2017-02-04 10:51:06 +0000

    Serialization and deserialization of models

--- minst.py
+++ minst.py
@@ -1,8 +1,9 @@
 from keras.datasets import mnist
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense
 from keras.utils import np_utils
 from keras.optimizers import SGD
+import os
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
@@ -15,22 +16,22 @@ test_images = test_images.astype('float32') / 255
 train_labels_oh = np_utils.to_categorical(train_labels)
 test_labels_oh = np_utils.to_categorical(test_labels)
 
-network = Sequential()
-network.add(Dense(input_dim=train_images.shape[1],
+model = Sequential()
+model.add(Dense(input_dim=train_images.shape[1],
                     output_dim=50,
                     init='uniform',
                     activation='tanh'))
 
-network.add(Dense(50))                    
-network.add(Dense(input_dim=50, 
+model.add(Dense(50))                    
+model.add(Dense(input_dim=50, 
                     output_dim=train_labels_oh.shape[1],
                     init='uniform',
                     activation='softmax'))
 
 sgd = SGD(lr=0.001, decay=1e-7, momentum=.9)
-network.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-network.fit(train_images, 
+model.fit(train_images, 
            train_labels_oh, 
            nb_epoch=50, 
            batch_size=300, 
@@ -38,5 +39,21 @@ network.fit(train_images,
            validation_split=0.1, 
            show_accuracy=True)
 
-print(network.metrics_names)
-print(network.evaluate(test_images, test_labels_oh, batch_size=16))
+print(model.metrics_names)
+print(model.evaluate(test_images, test_labels_oh, batch_size=16))
+
+
+model_json = model.to_json()
+with open("mnist_model.json", "w") as json_file:
+    json_file.write(model_json)
+
+h5file = "mnist_model.h5"
+model.save(h5file)    
+
+print("Reloading model and running on test data.")
+
+model_reload = load_model(h5file)
+print(model_reload.evaluate(test_images, test_labels_oh, batch_size=16))
+
+
+
